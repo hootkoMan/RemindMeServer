e2f417df7fcb3c17cb7b4d962dfaf276dc4cac52
ruslan <ruslan.bur@gmail.com>
2016-07-11 22:10:18 +0300

    Spring web configure. Added controller.

---
+++ src/main/java/com/remindme/server/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.remindme.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc //вкл режима веб мвс и рест кнтроллеров
+@ComponentScan("com.remindme.server")
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+}
